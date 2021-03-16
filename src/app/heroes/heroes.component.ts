a14ad270feae1a420a55875487c068aa6303d103
basil4ever <basil4ever@mail.ru>
2021-03-16 12:14:23 +0200

    tour of heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -16,6 +16,7 @@ export class HeroesComponent implements OnInit {
   ngOnInit(): void {
     this.getHeroes();
   }
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
     this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);

9172720e08896d320c709842e3c4480227549a79
basil4ever <basil4ever@mail.ru>
2021-03-18 10:41:31 +0200

    tour of heroes

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,21 +3,26 @@ import {Hero} from './hero';
 import {HEROES} from './moch-heroes';
 import {Observable, of} from 'rxjs';
 import {MessageService} from './message.service';
+import {HttpClient} from '@angular/common/http';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({  providedIn: 'root'})
 export class HeroService {
+  private heroesUrl = 'api/heroes';
+
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
     this.messageService.add('HeroService: fetched heroes');
     return heroes;
   }
-
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
 
   getHero(id: number): Observable<Hero> {
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(HEROES.find(hero => hero.id === id));
+  }
+  private log(message: string): void {
+    this.messageService.add(`HeroService: ${message}`);
   }
 }
